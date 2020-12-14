78c1401ad65305d63e6f6911cb90be8ddbb033d0
StoneBreakee <StoneBreakee@163.com>
2020-12-14 15:11:00 +0800

    提升运行速度
    String s = ".....";
    char[] arr = s.toCharArray()
    arr[i] 的速度优于 s.charAt(i)

--- dynamicprogram/[516]最长回文子序列.java
+++ dynamicprogram/[516]最长回文子序列.java
@@ -47,24 +47,28 @@ class Solution {
     // bbab 的回文子序列 bbb,bab
     // bbab 的回文子串 bab
     // 因此 bbbab 的回文子串 bbb,bab
-    //      bbbab 的回文子序列 bbbb
+    // bbbab 的回文子序列 bbbb
     // 以bbab为例
-    //    首 和 尾 都为 'b',则可以确定其回文 子 序列的长度为2
-    //       在看 ba 其最长回文子序列为 1
-    //   所以bbab 的最长回文子序列为 2 + 1 = 3
+    // 首 和 尾 都为 'b',则可以确定其回文 子 序列的长度为2
+    // 在看 ba 其最长回文子序列为 1
+    // 所以bbab 的最长回文子序列为 2 + 1 = 3
     public int longestPalindromeSubseq(String s) {
-        int[][] dp = new int[s.length()][s.length()];
-        for (int i = s.length() - 1; i >= 0; i--) {
+        int len = s.length();
+        int[][] dp = new int[len][len];
+        // 提升运行速度
+        char[] arr = s.toCharArray();
+        // 反着遍历保证状态的正确转移
+        for (int i = len - 1; i >= 0; i--) {
             dp[i][i] = 1;
-            for (int j = i + 1; j < s.length(); j++) {
-                if (s.charAt(i) == s.charAt(j)) {
+            for (int j = i + 1; j < len; j++) {
+                if (arr[i] == arr[j]) {
                     dp[i][j] = dp[i + 1][j - 1] + 2;
                 } else {
-                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                    dp[i][j] = dp[i + 1][j] > dp[i][j - 1] ? dp[i + 1][j] : dp[i][j - 1];
                 }
             }
         }
-        return dp[0][s.length() - 1];
+        return dp[0][len - 1];
     }
 }
 // leetcode submit region end(Prohibit modification and deletion)
